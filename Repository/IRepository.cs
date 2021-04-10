49af666f6c6cbf3a43d38a00b92c511e566ba624
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-10 11:00:19 +0100

    Adding a IRepository Interface with GenericMethods & class

---
+++ Repository/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Repository
+{
+    public interface IRepository<Tbl_Entity> where Tbl_Entity : class
+    {
+        IEnumerable<Tbl_Entity> GetAllRecords();
+        IQueryable<Tbl_Entity> GetAllRecordsIQueryable();
+        int GetAllRecordsCount();
+        void Add(Tbl_Entity entity);
+        void Update(Tbl_Entity entity);
+        void UpdateByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredicat, Action<Tbl_Entity> ForEachPredict);
+        Tbl_Entity GetFirstOrDefault(int recordId);
+        void Remove(Tbl_Entity entity);
+        void RemoveByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict);
+        void RemoveRangeByWhereClause(Expression<Func<Tbl_Entity, bool>> wherePredict);
+        void InactiveAndDeleteMarkByWhereCLause(Expression<Func<Tbl_Entity, bool>> wherePredict, Action<Tbl_Entity> ForEachPredict);
+        Tbl_Entity GetFirstOrDefaultByParameter(Expression<Func<Tbl_Entity, bool>> wherePredict);
+        IEnumerable<Tbl_Entity> GetListParameter(Expression<Func<Tbl_Entity, bool>> wherePredict);
+        IEnumerable<Tbl_Entity> GetResultBySQLProcedure(string query, params object[] parameters);
+    }
+}
