7eee582b475b63130286b96d30eb8ca125fe9518
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-10 11:45:14 +0100

    Adding the UnitOfWork Class

---
+++ Repository/GenericUnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using OnlineShoppingStore.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore.Repository
+{
+    public class GenericUnitOfWork : IDisposable
+    {
+        private dbMyOnlineShoppingEntities DBEntiy = new dbMyOnlineShoppingEntities();
+
+        public IRepository<Tbl_EntityType> GetRepositoryInstance<Tbl_EntityType>() where Tbl_EntityType : class
+        {
+            return new GenericRepository<Tbl_EntityType>(DBEntiy);
+        }
+
+        public void SaveChanges()
+        {
+            DBEntiy.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    DBEntiy.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool disposed = false;
+    }
+}
