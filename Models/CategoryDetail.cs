20f693cbfe2db6f99c9f0b486cc7c71edf918bf4
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-10 10:15:03 +0100

    Adding a CategoryDetail class to the Model

---
+++ Models/CategoryDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore.Models
+{
+    public class CategoryDetail
+    {
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Category Name is Required")]
+        [StringLength(100, ErrorMessage = "Minimum 3 and minimum 5 and maximum 100 character are allowed", MinimumLength = 3)]
+        public string CategoryName { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+    }
+}
