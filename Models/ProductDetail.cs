09c727d29852ce001966bb57f1759267c1bc9784
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-10 10:27:56 +0100

    Adding a ProductDetail class to the Model

---
+++ Models/ProductDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore.Models
+{
+    public class ProductDetail
+    {
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "Product Name is Required")]
+        [StringLength(100, ErrorMessage = "Minimum 3 and minimum 5 and maximum 100 character are allowed", MinimumLength = 3)]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(1, 50)]
+        public Nullable<int> CategoryId { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.DateTime> ModiifiedDate { get; set; }
+        [Required(ErrorMessage = "Description is required")]
+        public Nullable<System.DateTime> Description { get; set; }
+        public string ProductImage { get; set; }
+        public Nullable<bool> IsFeatured { get; set; }
+        [Required]
+        [Range(typeof(int), "1", "500", ErrorMessage = "Invalid Quantities")]
+        public Nullable<int> Quantity { get; set; }
+    }
+}
